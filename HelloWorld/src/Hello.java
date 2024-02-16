4ba271c80829686ff87f3a66217708a8793b0d15
Aaron Lewis <aaron11125@gmail.com>
2024-02-15 17:06:20 -0800

    More practice with conditionals

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -9,5 +9,17 @@ public class Hello {
         } else {
             System.out.println("It is an alien!");
         }
+
+        int topScore = 80;
+        if (topScore >= 100) {
+            System.out.println("You got the high score!");
+        } else {
+            System.out.println("You didn't get the high score!");
+        }
+
+        int secondTopScore = 60;
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greater than second top score and less than 100");
+        }
     }
 }
