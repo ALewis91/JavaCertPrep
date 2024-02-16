689b419f150a67f03d006bd7f5f11943ca9485d8
Aaron Lewis <aaron11125@gmail.com>
2024-02-16 12:14:46 -0800

    Practice with methods

--- IfKeywordAndCodeBlocks/src/MainChallenge.java
+++ IfKeywordAndCodeBlocks/src/MainChallenge.java
@@ -2,16 +2,19 @@ public class MainChallenge {
 
     public static void main(String[] args) {
 
-        boolean gameOver = true;
-        int score = 10000;
-        int levelCompleted = 8;
-        int bonus = 200;
+        calculateScore(false, 10000, 5, 100);
+        calculateScore(true, 800, 8, 200);
+    }
 
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         int finalScore = score;
 
         if (gameOver) {
             finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
             System.out.println("Your final score is " + finalScore);
+        } else {
+            System.out.println("Your score is " + finalScore);
         }
     }
 }
