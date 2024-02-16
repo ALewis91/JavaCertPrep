787e34d98cad0f238bc844790cdcef2a3fbe46f4
Aaron Lewis <aaron11125@gmail.com>
2024-02-15 16:59:30 -0800

    Practice if-then-else statements

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello, Aaron");
+
+        boolean isAlien = true;
+
+        if (!isAlien) {
+            System.out.println("It is not an alien!");
+        } else {
+            System.out.println("It is an alien!");
+        }
+    }
+}
